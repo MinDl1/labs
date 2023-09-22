d0df52cb6014b8f428e5d77c9534756eb0b0ff4f
MinDl <king.dud@mail.ru>
2023-09-22 18:06:37 +0300

    Update code

--- lab_3/code/Task_1-6/Task_1-6.ino
+++ lab_3/code/Task_1-6/Task_1-6.ino
@@ -24,35 +24,37 @@ LiquidCrystal_I2C lcd(0x27,16,2); // Задаем адрес и размерно
 //#define DHTTYPE DHT22 // DHT 22 (AM2302)
 //#define DHTTYPE DHT21 // DHT 21 (AM2301)
 //инициализация датчика
-DHT dht(DHTPIN, DHTTYPE);
 int t_w_numTones = 10;
 // Ноты C, C#, D, D#, E, F, F#, G, G#, A
 int t_w_tones[10] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
+DHT dht(DHTPIN, DHTTYPE);
 
 //gas
 int gas_numTones = 10;
 // Ноты C, C#, D, D#, E, F, F#, G, G#, A
-int gas_tones[10] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
+int gas_tones[10] = {400, 400, 400, 400, 400, 400, 400, 400, 400, 400};
 #define ANALOGPIN A1 //подключение аналогового сигнального пина
-MQ135 gasSensor = MQ135(ANALOGPIN);
 
 //flame
 int flame_numTones = 10;
 // Ноты C, C#, D, D#, E, F, F#, G, G#, A
-int flame_tones[10] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
+int flame_tones[10] = {300, 300, 300, 300, 300, 300, 300, 300, 300, 300};
 #define SENSOR_FLAME_PIN 4
 int flame = 0;
 
 //pump
 int pump_numTones = 10;
 // Ноты C, C#, D, D#, E, F, F#, G, G#, A
-int pump_tones[10] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
+int pump_tones[10] = {200, 200, 200, 200, 200, 200, 200, 200, 200, 200};
 #define PUMP_PIN 2
 #define WET_PIN A2
 int soilMoistureValue = 0;
 int percentage = 0;
 
-#define LED_DIOD 1
+#define LED_DIOD 12
+#define LED_DIOD1 8
+#define LED_DIOD2 9
+#define LED_DIOD3 10
 #define SOUND_PIN 6
 
 void setup()
@@ -62,25 +64,29 @@ void setup()
     lcd.createChar(1, degree); // Создаем символ под номером 1
 
 
-
     pinMode(SENSOR_FLAME_PIN, INPUT);
 
     pinMode(PUMP_PIN, OUTPUT);
+    pinMode(WET_PIN, INPUT);
 
     pinMode(LED_DIOD, OUTPUT);
+    pinMode(LED_DIOD1, OUTPUT);
+    pinMode(LED_DIOD2, OUTPUT);
+    pinMode(LED_DIOD3, OUTPUT);
     pinMode(SOUND_PIN, OUTPUT);
 
     Serial.begin(9600);
-    dht.begin();
 }
 void loop() {
     // Reading temperature or humidity takes about 250 milliseconds!
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    dht.begin();
     float h = dht.readHumidity();
     // Read temperature as Celsius
     float t = dht.readTemperature();
-    // Read temperature as Fahrenheit
-    float f = dht.readTemperature(true);
+    delay(3000);
+
+    lcd.clear();
     if(h > 10 && t > 30){
         // Выводим показания влажности и температуры
         lcd.setCursor(0, 0); // Устанавливаем курсор в начало 1 строки
@@ -93,89 +99,89 @@ void loop() {
         lcd.print(t,1); // Выводим значение температуры
         for (int i = 0; i < t_w_numTones; i++) {
             tone(SOUND_PIN, t_w_tones[i]);
-            digitalWrite(LED_DIOD, HIGH);
+            digitalWrite(LED_DIOD1, HIGH);
             delay(800);
-            digitalWrite(LED_DIOD, LOW);
         }
         noTone(SOUND_PIN);
     }
     else{
         // Выводим показания влажности и температуры
         lcd.setCursor(0, 0); // Устанавливаем курсор в начало 1 строки
-        lcd.print("Hum = % "); // Выводим текст
+        lcd.print("Hum = %  "); // Выводим текст
         lcd.setCursor(7, 0); // Устанавливаем курсор на 7 символ
         lcd.print(h, 1); // Выводим на экран значение влажности
         lcd.setCursor(0, 1); // Устанавливаем курсор в начало 2 строки
-        lcd.print("Temp = \1C "); // Выводим текст, \1 - значок градуса
+        lcd.print("Temp = \1C  "); // Выводим текст, \1 - значок градуса
         lcd.setCursor(7, 1); // Устанавливаем курсор на 7 символ
         lcd.print(t,1); // Выводим значение температуры
-        digitalWrite(LED_DIOD, LOW);
+        digitalWrite(LED_DIOD1, LOW);
     }
 
+    MQ135 gasSensor = MQ135(ANALOGPIN);
     float ppm = gasSensor.getPPM();
     float rzero = gasSensor.getRZero();
-    if(ppm > 20){
+    delay(2000);
+    lcd.clear();
+
+    if(ppm > 35){
         lcd.setCursor(0, 0);
-        lcd.print("ppm : % ");
+        lcd.print("ppm> : % ");
         lcd.print(ppm);
-        lcd.setCursor(7, 0);
-        lcd.print(" > 20");
         for (int i = 0; i < gas_numTones; i++) {
             tone(SOUND_PIN, gas_tones[i]);
-            digitalWrite(LED_DIOD, HIGH);
+            digitalWrite(LED_DIOD2, HIGH);
             delay(400);
-            digitalWrite(LED_DIOD, LOW);
         }
         noTone(SOUND_PIN);
     }
     else{
         lcd.setCursor(0, 0);
-        lcd.print("ppm : % ");
+        lcd.print("ppm< : %  ");
         lcd.print(ppm);
-        lcd.setCursor(7, 0);
-        lcd.print(" < 20");
-        digitalWrite(LED_DIOD, LOW);
+        digitalWrite(LED_DIOD2, LOW);
     }
 
+    lcd.clear();
     flame = digitalRead(SENSOR_FLAME_PIN);
+    delay(2000);
     if (flame == 1){
         lcd.setCursor(0, 0);
-        lcd.print("Есть Пламя");
+        lcd.print("Fire");
         for (int i = 0; i < flame_numTones; i++) {
             tone(SOUND_PIN, flame_tones[i]);
-            digitalWrite(LED_DIOD, HIGH);
+            digitalWrite(LED_DIOD3, HIGH);
             delay(200);
-            digitalWrite(LED_DIOD, LOW);
         }
         noTone(SOUND_PIN);
     } 
     else{
         lcd.setCursor(0, 0);
-        lcd.print("Нет Пламя");
-        digitalWrite(LED_DIOD, LOW);
+        lcd.print("No fire");
+        digitalWrite(LED_DIOD3, LOW);
     }
 
     soilMoistureValue = analogRead(WET_PIN);
     percentage = map(soilMoistureValue, 490, 1023, 100, 0);
-    Serial.println(percentage);
-    if(percentage < 10 || h < 10){
+    delay(2000);
+    lcd.clear();
+
+    if(percentage < 10 || h < 35){
         lcd.setCursor(0, 0);
-        lcd.print("Pump on");
-        digitalWrite(PUMP_PIN, LOW);
+        lcd.print("Pump on % ");
+        lcd.print(percentage);
+        digitalWrite(PUMP_PIN, HIGH);
         for (int i = 0; i < pump_numTones; i++) {
             tone(SOUND_PIN, pump_tones[i]);
             digitalWrite(LED_DIOD, HIGH);
             delay(100);
-            digitalWrite(LED_DIOD, LOW);
         }
         noTone(SOUND_PIN);
     }
-    if(percentage > 80 || h > 10){
+    if(percentage > 80 && h > 35){
         lcd.setCursor(0, 0);
-        lcd.print("Pump off");
-        digitalWrite(PUMP_PIN, HIGH);
+        lcd.print("Pump off % ");
+        lcd.print(percentage);
+        digitalWrite(PUMP_PIN, LOW);
         digitalWrite(LED_DIOD, LOW);
     }
-    
-    delay(1000);
 }
